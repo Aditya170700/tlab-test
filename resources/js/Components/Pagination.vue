cca16c1dcb589996dfd5272044fe849914975d39
Aditya170700 <adityaric72@gmail.com>
2023-06-28 21:15:15 +0700

    Pagination

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,15 +1,15 @@
 <template>
     <div v-if="links.length > 3">
-        <div class="flex flex-wrap mt-8">
+        <div class="flex flex-wrap items-center mt-8">
             <div v-for="(link, i) in links" :key="i">
                 <div
                     v-if="link.url === null"
-                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
+                    class="mr-1 px-4 py-2 text-sm leading-4 text-gray-400 border rounded-md"
                     v-html="link.label"
                 />
                 <Link
                     v-else
-                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-primary focus:text-primary"
+                    class="mr-1 px-4 py-2 text-sm leading-4 border rounded-md hover:bg-white hover:text-gray-800 focus:border-primary focus:text-primary"
                     :class="{ 'bg-blue-700 text-white': link.active }"
                     :href="link.url"
                 >
